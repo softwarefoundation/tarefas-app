8077d0a88f03988af854f3bc853e23a2167fe3da
Dherkyan Ribeiro da Silva <dherkyan@gmail.com>
2021-05-07 15:56:05 -0400

    17. Trabalhando com formulários

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tarefas-app';
+
+  tarefaForm: FormGroup = new FormGroup({
+    descricao: new FormControl('')
+  });
+
+  private cadastrarTarefa(): void {
+    console.log(this.tarefaForm.value);
+  }
+
 }
